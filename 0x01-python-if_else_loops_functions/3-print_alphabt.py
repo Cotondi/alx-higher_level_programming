f2fd3464df31d04e52c0ac0d5c509ef99fb865a3
Cotondi <nundaclinton@gmail.com>
2023-11-29 05:01:37 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for chars in range(ord('a'), (ord('z')) + 1):
     if chr(chars) not in ['q', 'e']:
-      print("{:}".format(chr(chars)), end='')
+        print("{:}".format(chr(chars)), end='')
