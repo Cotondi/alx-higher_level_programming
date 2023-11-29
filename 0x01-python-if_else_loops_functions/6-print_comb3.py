1ebd8d217aafef3307457bab0ca5611a0cd1bc23
Cotondi <nundaclinton@gmail.com>
2023-11-29 12:03:13 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for o in range(9):
     for t in range(o + 1, 10):
-        print("{:d}{:d}".format(o, t), end=(", " if not (o == 8 and t == 9) else "\n"))
+        print("{:d}{:d}".format(o, t), end=(", " if not (o == 8 and t == 9)
+            else "\n"))
