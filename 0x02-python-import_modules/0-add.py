c01b52384fe9650e5738adb7b549ead2479d85b6
Cotondi <nundaclinton@gmail.com>
2023-12-01 14:52:49 -0800

    1-calculation.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+from add_0.py import add(a, b):
 a = 1
 b = 2
-add_0 = __import__(add_0)
-print("{} + {} = {}" .format(a, b, add_0.add(a, b)))
+add_0 ={}
+exec(open("add_0.py") .read(), add_0)
+print("{:d} + {:d} = {:d}" .format(a, b, add_0['add'](a,b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1 as c
+a = 10
+b = 5
+print(c.add(a, b))
+print(c.sub(a, b))
+print(c.mul(a, b))
+print(c.div(a, b))
